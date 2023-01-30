6cb366380f181d9fc806cfb9ac13940a802bc51f
Emerson Matos <emegson@live.com>
2023-01-30 20:24:10 -0300

    primeira tentativa

--- runweb.sh
+++ runweb.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 
 echo "Open http://localhost:8080/"
 
-# Serve files using a local webserver
-emrun --no_browser --port 8080 public/
+# Serve files using a webserver
+# emrun --no_browser --port 8080 public/
+python -m http.server -d public 8080
